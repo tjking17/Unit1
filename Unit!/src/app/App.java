927163b86f9cc8c5185cafa026d425304551dad8
tjking17 <827670@mystma.org>
2020-09-14 16:50:48 +0000

    Added top five movies

--- Unit!/src/app/App.java
+++ Unit!/src/app/App.java
@@ -2,6 +2,11 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Notebook");
+        System.out.println("2.Toystory");
+        System.out.println("3.Wall-e");
+        System.out.println("4. Annabelle");
+        System.out.println("5. The conjuring");
+
     }
 }
