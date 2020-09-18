ae74f7fb962a5217f5afefbe58ec0ee6c4057993
tjking17 <827670@mystma.org>
2020-09-18 13:34:58 +0000

    created java

--- Unit!/src/app/App.java
+++ Unit!/src/app/App.java
@@ -1,12 +1,48 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("1. Notebook");
-        System.out.println("2.Toystory");
-        System.out.println("3.Wall-e");
-        System.out.println("4. Annabelle");
-        System.out.println("5. The conjuring");
+    public static void main(final String[] args) throws Exception {
+        final Boolean hasAPet = false;
+        final char lastLetterofName = 'K';
+              byte howManycountries= (byte) 165;
+              short Bignumber= 100;
+              int myFavNum= 3609;
+              long myLeastfavNum= 1775093;
+              float decimalNum= (float) 12.22;
+              Double howMuchisDouble= (double) 22;
 
-    }
-}
+             
+              
+            
+               
+            
+
+
+    
+       System.out.println(hasAPet);
+       System.out.println("Do I have a Pet?");
+       System.out.println(lastLetterofName);
+       System.out.println("The last letter of my name is:");
+       System.out.println(howManycountries);
+       System.out.println("There are 165");
+       System.out.println(Bignumber);
+       System.out.println("The big number for short is 100?");
+       System.out.println(myFavNum);
+       System.out.println("my fav number is:");
+       System.out.println(myLeastfavNum);
+       System.out.println("my least fav num is:");
+       System.out.println(decimalNum);
+       System.out.println("the decimal number is:");
+       System.out.println(howMuchisDouble);
+       System.out.println("its worth more then:");
+
+
+    
+
+
+
+       
+    
+    
+
+    }}
