fa5483b674ba123cc3b49ec61cea53ca1027658d
tjking17 <827670@mystma.org>
2020-09-25 15:18:24 +0000

    Mad libs part two

--- Unit!/src/app/App.java
+++ Unit!/src/app/App.java
@@ -1,37 +1,28 @@
 package app;
+import java.util.Scanner;
 
-import javafx.scene.effect.BlurType;
 
 public class App {
 
-    public static void main(final String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
        
-    String person1;
-    String person2;
-    String verb1;
-    String verb2;
-    String verb3;
-    String place1;
-    String adjective;
-    String noun;
-    String noun2;
+    Scanner input = new Scanner(System.in);
+
+System.out.println("Please enter a noun");
+String noun = input.nextLine();
+System.out.println("please enter second noun");
+String noun2 = input.nextLine();
+System.out.println("please enter in a verb");
+String verb = input.nextLine();
+System.out.println("please enter in second verb");
+String verb2 = input.nextLine();
+System.out.println("please eneter in a name");
+String name = input.nextLine();
 
 
 
 
-
-
-    System.out.println("Jerry+ is a programmar who likes to" + "bake");
-    System.out.println("Jerry was a chef that liked pizza");
-    System.out.println("to make a pizza you need to smooth it out");
-    System.out.println("and make the thin rough crust");
-    System.out.println("then you cover it with red pastey sauce");
-    System.out.println("roughly cheese, and freshly cut peperonni");
-    System.out.println("next you have to bake it in a very hot oven");
-    System.out.println("when its done, cut it ");
-    System.out.println("into 12 triangles" );
-    System.out.println("some kids like cheese pizza the best, but my favorite:");
-    System.out.println("is pineapple pizza, if I could, I would eat pizza 100 times a day:");
+   
 
 
 		
