74b184aa679b5d2811f98c695c8656541977707c
tjking17 <827670@mystma.org>
2020-09-28 20:37:17 +0000

    Redo of while loop

--- Unit!/src/app/App.java
+++ Unit!/src/app/App.java
@@ -7,20 +7,21 @@ public class App {
     public static void main(final String[] args) throws Exception {
 int count = 0;
 String color = "blue";
-   
+boolean person = true;
+
 while (count<= 10);
 System.out.println("the count is"+ count);
 		count= count + 2;
 while (color == "blue"){
       System.out.println("The color is"+ color);
         color = "red";
-}
+
+} 
+ System.out.println("Finished");
+    while (person== false);
+    System.out.println("false");
+    System.out.println("Finished");
     
-    System.out.println("Person finsihed");
-        String person;
-    System.out.println("the person is person");
-        person = "tyler";
-        System.out.println("Person finsihed");
     }
 
         
