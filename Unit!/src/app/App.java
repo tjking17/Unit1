f167637ad55932678050b8716837173cd9dffefe
tjking17 <827670@mystma.org>
2020-10-01 16:57:03 +0000

    for loops attempt

--- Unit!/src/app/App.java
+++ Unit!/src/app/App.java
@@ -5,43 +5,36 @@ package app;
 public class App {
 
     public static void main(final String[] args) throws Exception {
-int count = 0;
-String color = "blue";
-boolean person = true;
+     
+        for(int i = 0; i <=10; i = i +1){
+        System.out.println("The count is" + i);
+        System.out.println("Lets keep  counting.");
+        }
+       for(int i = 10; i <20; i = i + 1){
+           System.out.println("The count is" + i);
+           System.out.println("Lets keep counting.");
+       }
+       for(int i = 10; i <100; i = i + 1){
+           System.out.println("The count is" + i);
+           System.out.println("Lets keep counting.");
 
-while (count<= 10);
-System.out.println("the count is"+ count);
-		count= count + 2;
-while (color == "blue"){
-      System.out.println("The color is"+ color);
-        color = "red";
-
-} 
- System.out.println("Finished");
-    while (person== false);
-    System.out.println("false");
-    System.out.println("Finished");
-    
-    }
-
-        
-
-        
-
-        
-
+       }
+       for(int i = 1000; i <2625; i = i + 1){
+           System.out.println("The count is" + 1);
+           System.out.println("Lets keep counting.");
+       }
+        for(int i =11; i <12; i = i + 1){
+            System.out.println("The count is" + i);
+            System.out.println("Lets count down");
+        }
+        for(int i =10; i <0; i = i -1){
+            System.out.println("The count is" + i);
+            System.out.println("lets keep counting.");
 
 
       
-      
-      
-    
-    
-
-
-
-       
-    
-    
-
+    }  
 }
+}
+   
+	
