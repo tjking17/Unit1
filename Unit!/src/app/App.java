8f4949a366e76f17abd65b28906fa2ada597151b
tjking17 <827670@mystma.org>
2020-10-08 15:28:42 +0000

    Finished

--- Unit!/src/app/App.java
+++ Unit!/src/app/App.java
@@ -1,52 +1,57 @@
 package app;
+
 import java.util.Scanner;
 
-
-public class App {
+public class App{
 
     public static void main(final String[] args) throws Exception {
-int count = 0;
-String color = "blue";
-boolean person = true;
-Scanner input = new Scanner(System.in);
+    
+      Scanner input = new Scanner(System.in);
+      System.out.println("Welcome to dog facts(please choose a fact 1-5) ");
+      String fact1 = input.nextLine();
+      System.out.println("Dogs can have healthy tongues");
+      System.out.println(" Dogs have a lifespan of 11years");
+      String fact2 = input.nextLine();
+      System.out.println("Forty-five percent of u.s dogs sleep in thier owners bed");
+      String fact3 = input.nextLine();
+      System.out.println("Seventy percent of people sign thier dogs name on thier holiday cards.");
+      String fact4 = input.nextLine();
+      System.out.println("A dogs nose print is unique, as much like a persons finger print.");
+      String fact5 = input.nextLine();
+      System.out.println("A dogs sense of smell is legendary");
+      
 
-System.out.println("Enter a color");
-int number = 10;
 
 
-while (count<= 10);
-System.out.println("the count is"+ count);
-		count= count + 2;
-while (color == "blue"){
-      System.out.println("The color is"+ color);
-        color = "red";
-
-} 
- System.out.println("Finished");
-    while (person== false);
-    System.out.println("false");
-    System.out.println("Finished");
     
     }
 
-        
 
         
+      
 
-        
+
+
+
 
 
 
       
-      
-      
+            
+
     
-    
-
-
-
-       
     
     
 
-}
+        
+
+    
+
+
+        
+
+ {       
+
+        
+
+    }}
