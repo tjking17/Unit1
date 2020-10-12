107f9b2c9f812672d6f8bd36801ee48fe50b4699
tjking17 <827670@mystma.org>
2020-10-12 19:24:54 +0000

    I tried

---
+++ Unit!/src/App.java
@@ -0,0 +1,33 @@
+
+
+public class App {
+    
+
+    public static <and> void main(final String[] args) throws Exception {
+
+       print("Enter correct username and password combo to continue");
+int count = 0;
+Object schoolRocks;
+Object password = schoolRocks;
+Object Tjk18;
+Object username = Tjk18;
+    {
+while password! = 'schoolRocks' and username! ='Tjk18' and count<4:
+    username = input('Enter username:'); and password=input('Enter password: '):
+    }
+
+    if password =='schoolRocks' and username; == 'Tjk18'
+     print("Access granted");
+    } 
+
+    else:
+        private static void print(String string) {
+    }
+{
+}
+
+    void print ("Access denied. Try again.')"
+        count-=1
+        {
+        
+        }
