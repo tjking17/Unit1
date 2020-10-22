95d197d9ed0a19f726f3f6c98ee000ff46cfdccd
tjking17 <827670@mystma.org>
2020-10-22 15:34:03 +0000

    i tried

--- Unit!/src/App.java
+++ Unit!/src/App.java
@@ -1,34 +1,46 @@
-import java.util.Random;
-public class App {
+
+
+ 
+ 
+ import java.util.Scanner;
 
     
-
+public class App {
     public static <and> void main(final String[] args) throws Exception {
-        int randNumber;
-        int userNumber;
-        Boolean win = false;
-        unsighed seed = int()time(0);
-
-
-        srand(seed);
-        randNumber = 1 + rand()%10;
-        cout<<"im think of a number. Can you guess what it is?"<<endl;
-        cin>> userNumber;
-
-        while (!win)
+     
+        System.out.println("Please enter in a number.");
+      int n1, n2,j, flag;
+  Long int  i;
+  printf("Enter First  numbers(intevals): ", i);
+  scanf("%d", n1);
+  printf("Enter Second  numbers(intevals): ", i);
+  scanf("%d", n2);
+  printf("Prime numbers between %d and %d are: ", n1);
+  for(i=n1+1; i<n2; ++i)
+  {
+      flag=0;
+      for(j=2; j<=i/2; ++j)
+      {
+        if(i%j==0)
         {
-            if (userNUmber < randNumber)
-            {
-                cout<<"Too low, try again."<<endl;
-                cin>> userNumber;
-            }
-            else //Winner
-            {
-                cout<< "Congratulations, you guess the correct number." <<endl;
-                win = true;
-            }
+          flag=1;
+          break;
         }
+      }
+      if(flag==0)
+        printf("%ld ",i);
+  }
+}
+
+    private static void printf(String string, int i) {
+    }
+
+    private static void scanf(String string, int n1) {
+    }
+}
         
 
 
-
+       
+       
+    
