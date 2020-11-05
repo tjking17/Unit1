3a17562a2540096cb83f015503afe36de3d383b7
tjking17 <827670@mystma.org>
2020-11-05 15:10:15 +0000

    done

--- Unit!/src/App.java
+++ Unit!/src/App.java
@@ -8,34 +8,10 @@
 public class App {
     public static <and> void main(final String[] args) throws Exception {
      
-        System.out.println("Please enter in a number.");
-      int n1, n2,j, flag;
-  Long int  i;
-  printf("Enter First  numbers(intevals): ", i);
-  scanf("%d", n1);
-  printf("Enter Second  numbers(intevals): ", i);
-  scanf("%d", n2);
-  printf("Prime numbers between %d and %d are: ", n1);
-  for(i=n1+1; i<n2; ++i)
-  {
-      flag=0;
-      for(j=2; j<=i/2; ++j)
-      {
-        if(i%j==0)
-        {
-          flag=1;
-          break;
-        }
-      }
-      if(flag==0)
-        printf("%ld ",i);
-  }
-}
+      int sally[]={2,3,4,5,6,7};
 
-    private static void printf(String string, int i) {
-    }
+      System.out.println(sally[2]);
 
-    private static void scanf(String string, int n1) {
     }
 }
         
