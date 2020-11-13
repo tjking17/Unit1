484e20711ca2f10492384641ccb2abfaeeb6ec15
tjking17 <827670@mystma.org>
2020-11-13 18:42:33 +0000

    challenge 4

--- Unit!/src/App.java
+++ Unit!/src/App.java
@@ -4,34 +4,26 @@ public class App {
     public static <and> void main(final String[] args) throws Exception {
      
      Scanner input = new Scanner(System.in);
-     String password = "Rocks2020";
-     int tries = 0;
-     boolean secretMessageGiven = false;
 
-     while (tries <3) {
-         if (secretMessageGiven == false) {
-             System.out.println("Enter the password");
-             String enteredPassword = input.nextLine();
+     String number1;
+     int number2;
+     String sum;
 
-             if (enteredPassword.equals(password)) {
-                 System.out.println("ACCESS GRANTED");
-                 System.out.println("DONT FORGET TO SLEEP");
-                 secretMessageGiven = true;
-             }
+     System.out.println("Enter first integer");
+     number1 = input.nextLine();
 
-             else {
-                 System.out.println("ACCESS DENIED");
-                 tries++;
-                 System.out.println("YOU HAVE" + (3 - tries) + "ATTEMPTS REMAINING");
-             }
-            }
-        }
-        if (tries == 3) {
-            System.out.println("TOO MANY ATTEMPTS");
-        }
+     System.out.println("Enter seconf integer");
+     number2 = input.nextInt();
+
+     sum = number1 + number2;
     }
 }
-             
+
+
+    
+
+
+    
     
     
 
